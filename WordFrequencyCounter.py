51de7a05f7e928c2b05625e63ecb278a59bd815f
Guy Bitan <guy.bitan@gmail.com>
2022-01-04 19:35:59 +0200

    add

--- WordFrequencyCounter.py
+++ WordFrequencyCounter.py
@@ -15,4 +15,26 @@ def start(url):
             word_list.append(each_word)
 
 
-start('')
+def clean_up_list(word_list):
+    clean_word_list = []
+    for word in word_list:
+        symbols = "!@#$%^&*()_+{}:\"<>?,./;[]-='"
+    for i in range(0, len(symbols)):
+        word = word.replace(symbols[i], "")
+    if len(word) > 0:
+        clean_word_list.append(word)
+    create_dictionary(clean_up_list)
+
+
+def create_dictionary(clean_word_list):
+    word_count = {}
+    for word in clean_up_list:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    for key, value in sorted(word_count.items(), key=operator.itemgetter(1)):
+        print(key, value)
+
+
+start('https://...')
