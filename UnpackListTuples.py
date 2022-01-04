2b5eca56a26f7f8bda2dac28b6766dc7571c2e53
Guy Bitan <guy.bitan@gmail.com>
2022-01-04 20:11:01 +0200

    add

--- UnpackListTuples.py
+++ UnpackListTuples.py
@@ -1,4 +1,15 @@
 # Each item in the list will be set to different variable
-date, item, price = ['December 23, 2015', 'Guy Bitan', '8.51']
+date, name, price = ['December 23, 2015', 'Guy Bitan', '8.51']
 print(name)
+print(date)
+print(price)
 
+
+def drop_first_last(grades):
+    # Take the first item and list and save in the first var all the items in the middles and store in middle
+    first, *middle, last = grades
+    avg = sum(middle) / len(middle)
+    print(avg)
+
+
+drop_first_last([65, 76, 98, 54, 21])
---
+++ Zip.py
@@ -0,0 +1,10 @@
+first = ['Guy', 'Tom', 'Taylor']
+last = ['Bitan', 'Nativ', 'Golan']
+
+# The results is map
+names = zip(first, last)
+# Results
+# [('Guy','Bitan'),('Tom','Nativ'),('Taylor','Golan')]
+
+for a, b in names:
+    print(a, b)
