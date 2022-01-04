7785d84d2654b23101c39ddc35cb7dc85e100af7
Guy Bitan <guy.bitan@gmail.com>
2022-01-04 09:22:48 +0200

    add

--- ReadWriteFiles.py
+++ ReadWriteFiles.py
@@ -1,8 +1,9 @@
+# Open file for write and create it
 fw = open('sample.txt', 'w')
 fw.write('writing some stuff in my text file\n')
 fw.write('i lobe python\n')
 fw.close()
-
+# Open file for read
 fr = open('sample.txt', 'r')
 text = fr.read()
 print(text)
